bf56d5ef617d60ed8fef2ec56edede63c0a57ab8
Raphaël Duhen <raphael.duhen.nexus@gmail.com>
2021-08-13 22:40:38 +0200

    feat(world): Created basic SchoolInfo and auxiliary classes.

---
+++ Assets/Scripts/World/SchoolInfo.cs
@@ -0,0 +1,44 @@
+using System.ComponentModel;
+
+namespace World
+{
+    public class Coordinates
+    {
+        private double longitude;
+        private double latitude;
+    }
+
+    public class Socials
+    {
+        private string instagram;
+        private string twitter;
+        private string facebook;
+    }
+
+    public class Resume
+    {
+        private string approxAddress;
+        private int since;
+        private int numberOfStudents;
+        private int numberOfForeigners;
+        private string history;
+        private string[] majors;
+        private string mailAddress;
+        private long phone;
+    }
+    
+    public class SchoolInfo
+    {
+        private int id;
+        private int ranking;
+        private string name;
+        private string category;
+        private bool intramuros;
+        private Coordinates coordinates;
+        private int womanprop;
+        private string imagepath;
+        private string website;
+        private int fees;
+        
+    }
+}
